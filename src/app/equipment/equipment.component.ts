7f660210341f92994c4ea7d076f43848301653fd
kmazreku <64272109+kmazreku@users.noreply.github.com>
2020-05-26 21:16:49 -0400

    Completed Studio

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-equipment',
-  templateUrl: './equipment.component.html',
-  styleUrls: ['./equipment.component.css']
+  selector: "app-equipment",
+  templateUrl: "./equipment.component.html",
+  styleUrls: ["./equipment.component.css"],
 })
 export class EquipmentComponent implements OnInit {
+  equipment: string[] = [
+    "Habitat dome",
+    " Drones",
+    "Food containers",
+    "Oxygen tanks",
+  ];
+  toolBeingEdited: string = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  addEquipment(tools: string) {
+    this.equipment.push(tools);
   }
-
+  editEquipment(item: string) {
+    this.toolBeingEdited = item;
+  }
 }
