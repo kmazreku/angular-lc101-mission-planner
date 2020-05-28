5dfce0f915bbec7a0808b25a4825cc0255505739
kmazreku <64272109+kmazreku@users.noreply.github.com>
2020-05-28 19:33:31 -0400

    Studio 3 Complete

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,21 +6,37 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./equipment.component.css"],
 })
 export class EquipmentComponent implements OnInit {
-  equipment: string[] = [
-    "Habitat dome",
-    " Drones",
-    "Food containers",
-    "Oxygen tanks",
+  equipmentItems: object[] = [
+    { name: "Duct Tape", mass: 0.5 },
+    { name: "Space Camera", mass: 20 },
+    { name: "Food", mass: 150 },
+    { name: "Oxygen Tanks", mass: 400 },
+    { name: "AE-35 Unit", mass: 5 },
+    { name: "ISS Supplies", mass: 800 },
+    { name: "Water", mass: 250 },
+    { name: "Satellite", mass: 1200 },
+    { name: "R2 Unit", mass: 32 },
   ];
-  toolBeingEdited: string = null;
+  cargoHold: object[] = [];
+  cargoMass: number = 0;
+  maximumAllowedMass: number = 2000;
+  maxItems: number = 10;
 
   constructor() {}
 
   ngOnInit() {}
-  addEquipment(tools: string) {
-    this.equipment.push(tools);
+  // Code your addItem function here:
+  addItem(equipment: { name; mass }): boolean {
+    this.cargoHold.push(equipment);
+    this.cargoMass += equipment.mass;
+    if (this.maximumAllowedMass - this.cargoMass <= 200) {
+      return true;
+    }
+    return false;
   }
-  editEquipment(item: string) {
-    this.toolBeingEdited = item;
+
+  emptyHold() {
+    this.cargoMass = 0;
+    this.cargoHold = [];
   }
 }
